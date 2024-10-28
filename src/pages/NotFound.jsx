aa81cae42f39ab270e9c3c1daec64b9dd14e73e9
unixnexo <unixnexo@gmail.com>
2024-10-28 23:49:49 +0330

    dev

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-    return ( <h1>why u here bro!</h1> );
+    return (
+        <div className="flex flex-col justify-center items-center mt-20 space-y-4">
+            <img className="size-[50%] rounded-xl" src="./The_Krusty_Krab.png" alt="funny picture" />
+            <h1 className="text-3xl">Wrong turn! Go back to <Link to={'/'} className="text-Bred font-bold underline underline-offset-8">Menu</Link></h1>
+        </div>
+    );
 }
  
 export default NotFound;
