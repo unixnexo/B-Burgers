467673875233243db2ac207db4bfae48a12b2410
unixnexo <unixnexo@gmail.com>
2024-11-01 00:29:10 +0330

    ded

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -2,18 +2,20 @@ import { Link } from "react-router-dom";
 
 const NavbarItem = ({ title, imgSrc, isActive, onClick, notCategory, route }) => {
 
-    return ( 
-        <Link to={route}>
-            <div onClick={onClick} className={"flex shrink-0 flex-col lg:flex-row justify-center lg:justify-start items-center lg:space-x-2 mx-3 lg:mx-0 cursor-pointer lg:hover:scale-105 transition-all"}>
-                <div className={`size-20 grow lg:grow-0 overflow-hidden lg:rounded lg:border-t-0 border-Bred lg:pl-4 lg:py-2 transition-all duration-100 ${isActive ? 'lg:border-l-4 border-t-2' : 'border-0'}`}>
-                    {imgSrc ? <img src={imgSrc} className="w-full object-cover" alt="burgers" /> : <p className="flex justify-center items-center size-full text-2xl text-Bred">0$</p>}
+    return (
+        <div>
+            <Link to={route}>
+                <div onClick={onClick} className={"flex shrink-0 flex-col lg:flex-row justify-center lg:justify-start items-center lg:space-x-2 mx-3 lg:mx-0 cursor-pointer lg:hover:scale-105 transition-all"}>
+                    <div className={`size-20 grow lg:grow-0 overflow-hidden lg:rounded lg:border-t-0 border-Bred lg:pl-4 lg:py-2 transition-all duration-100 ${isActive ? 'lg:border-l-4 border-t-2' : 'border-0'}`}>
+                        {imgSrc ? <img src={imgSrc} className="w-full object-cover" alt="burgers" /> : <p className="flex justify-center items-center size-full text-2xl text-Bred">0$</p>}
+                    </div>
+                    <div className="lg:pr-4 text-sm lg:text-base">
+                        <p>{title}</p>
+                    </div>
                 </div>
-                <div className="lg:pr-4 text-sm lg:text-base">
-                    <p>{title}</p>
-                </div>
-            </div>
-            {notCategory && <hr className="w-3/4 mx-auto hidden lg:block" />}
-        </Link>
+                {notCategory && <hr className="w-3/4 mx-auto hidden lg:block" />}
+            </Link>
+        </div>
     );
 }
  
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 const MainLayout = ({ initialMenuItems, setMenuItems }) => {
+    const [animationParent] = useAutoAnimate();
 
     return (
         <>
             <Header />
 
             <main>
-                <div className="flex lg:flex-row flex-col p-2 pt-8 pb-20">
+                <div className="flex lg:flex-row flex-col p-2 pt-8 pb-20" ref={animationParent}>
 
                     {/* sidebar */}
                     <div className="lg:border-2 rounded-lg ml-2 h-min lg:sticky top-3 mb-16 lg:mb-0 flex justify-center">
@@ -17,7 +19,7 @@ const MainLayout = ({ initialMenuItems, setMenuItems }) => {
                     </div>
 
                     {/* main content */}
-                    <Outlet />
+                    <Outlet  />
 
                 </div>
             </main>
