af8aaf8e8e3efef9a06e1592cbb902bd7d2dd288
unixnexo <unixnexo@gmail.com>
2024-11-03 21:46:21 +0330

    dev

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
         <div className="flex flex-col justify-center items-center mt-20 space-y-4">
-            <img className="size-[50%] rounded-xl" src="./The_Krusty_Krab.png" alt="funny picture" />
-            <h1 className="text-xl md:text-3xl">Wrong turn! Go back to <Link to={'/'} className="text-Bred font-bold underline underline-offset-8">Menu</Link></h1>
+            <img className="size-[90%] sm:size-[70%] md:size-[50%] rounded-xl" src="./The_Krusty_Krab.png" alt="funny picture" />
+            <h1 className="text-base sm:text-xl md:text-3xl">Wrong turn! Go back to <Link to={'/'} className="text-Bred font-bold underline underline-offset-8">Menu</Link></h1>
         </div>
     );
 }
--- src/pages/Receipt.jsx
+++ src/pages/Receipt.jsx
@@ -1,22 +1,46 @@
 const Receipt = ({ userReceipt, totalPrice }) => {
-    return (
-        <div className="mx-auto space-y-5">
-            {userReceipt.map((item) => (
-                <div key={item.id} className="flex justify-between min-w-[600px] text-xl px-3 pb-4 bg-white shadow-xl rounded-xl overflow-hidden">
-                    <div className="flex space-x-2 overflow-hidden">
-                        <div className="size-36 overflow-hidden">
-                            <img src={item.imgSrc} className="object-cover size-full" alt={item.title} />
+    if (totalPrice !== 0) {
+        return (
+            <div className="mx-auto space-y-5">
+    
+                {userReceipt.map((item) => (
+                    <div key={item.id} className="flex justify-between min-w-[600px] text-xl px-3 pb-4 bg-white shadow-xl rounded-2xl overflow-hidden">
+                        <div className="flex space-x-2 overflow-hidden">
+                            <div className="size-36 overflow-hidden">
+                                <img src={item.imgSrc} className="object-cover size-full" alt={item.title} />
+                            </div>
+                            <div className="self-end mb-4">
+                                <p>x{item.quantity}</p>
+                                <p>{item.title}</p>
+                            </div>
                         </div>
-                        <div className="self-end mb-4">
-                            <p>x{item.quantity}</p>
-                            <p>{item.title}</p>
+                        <div className="self-end mb-4 text-Bred/80 pr-3">
+                            <p>${item.price}</p>
                         </div>
                     </div>
-                    <div className="self-end mb-4 text-Bred/80">
-                        <p>{item.price}$</p>
+                ))}
+    
+                <br/>
+                <hr />
+                
+                <div className="flex justify-between">
+                    <div className="text-Bred text-3xl font-bold">
+                        <p>${totalPrice}</p>
+                    </div>
+                    <div className="flex space-x-3 *:py-2.5 *:w-36 *:rounded-2xl">
+                        <button className="bg-Bred text-white">Pay Online</button>
+                        <button className="bg-white shadow-md border">Pay at Counter</button>
                     </div>
                 </div>
-            ))}
+    
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex flex-col justify-center items-center grow">
+            <img src="./public/noBurger.avif" className="rounded-xl" alt="no burgers" />
+            <p className="mt-3 text-base sm:text-xl md:text-2xl text-center">You haven't picked any burger yet!</p>
         </div>
     );
 }
